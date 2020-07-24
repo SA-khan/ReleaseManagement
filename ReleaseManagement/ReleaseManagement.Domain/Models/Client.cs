1bdf43a1ba41b495afb5c48b44c0b7477a787f44
Saad Ahmed <saadahmed7262@gmail.com>
2020-07-24 23:25:53 +0500

    Add Client Domain Model

---
+++ ReleaseManagement/ReleaseManagement.Domain/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReleaseManagement.Domain.Models
+{
+    class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+}
